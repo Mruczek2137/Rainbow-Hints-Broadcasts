563af2f09fa9696e0d17224d0c270438b4a7166e
Mruczek2137 <96917262+Mruczek2137@users.noreply.github.com>
2024-06-22 13:36:22 +0200

    Dodaj pliki projektów.

---
+++ PluginReload/Config.cs
@@ -0,0 +1,17 @@
+﻿using Exiled.API.Interfaces;
+using System.ComponentModel;
+
+namespace RainbowBroadcastPlugin
+{
+    public class Config : IConfig
+    {
+        public bool IsEnabled { get; set; } = true;
+        public bool Debug { get; set; } = false;
+
+        [Description("The message to display as a rainbow broadcast.")]
+        public string BroadcastMessage { get; private set; } = "Good luck";
+
+        [Description("The duration of the rainbow broadcast in seconds.")]
+        public int BroadcastDuration { get; private set; } = 10;
+    }
+}
---
+++ PluginReload/Handler.cs
@@ -0,0 +1,29 @@
+﻿using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using MEC;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RainbowBroadcastPlugin
+{
+    public class EventHandlers
+    {
+        private readonly string[] colors = { "red", "orange", "yellow", "green", "blue", "purple" };
+
+        public void OnRoundStarted()
+        {
+            Timing.RunCoroutine(SendRainbowBroadcast(Plugin.Instance.Config.BroadcastMessage, Plugin.Instance.Config.BroadcastDuration));
+        }
+
+        public IEnumerator<float> SendRainbowBroadcast(string message, int duration)
+        {
+            for (int i = 0; i < duration * 2; i++) // Changing color every 0.5 seconds
+            {
+                string currentColor = colors[i % colors.Length];
+                string rainbowMessage = $"<color={currentColor}>{message}</color>";
+                Map.Broadcast(1, rainbowMessage);
+                yield return Timing.WaitForSeconds(0.5f);
+            }
+        }
+    }
+}
